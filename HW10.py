35b34dc8add0d7b722f5e0851b868414974e8330
SanTisLugansk <alex.tischik@i.ua>
2023-06-21 00:56:27 +0300

    ДЗ 10 виконано

--- HW10.py
+++ HW10.py
@@ -4,14 +4,20 @@
 
 class vehicle:
     weight = None
-    speed = None
+    max_speed = None
     engine_power = None
-    length = None
-    width = None
-    height = None
+    _owner = None
 
 class Car(vehicle):
     color = None
+    __is_electric = False
+
+    def __init__(self, max_speed, color):
+        self.max_speed = max_speed
+        self.color = color
+
+    def get_is_electric(self):
+        return self.__is_electric
 
 class Airplane(vehicle):
     maximum_height = None
@@ -19,3 +25,19 @@ class Airplane(vehicle):
 
 class Ship(vehicle):
     displacement = None
+
+    def __init__(self, weight, max_speed, engine_power, owner=None):
+        self.weight = weight
+        self.max_speed = max_speed
+        self.engine_power = engine_power
+        self._owner = owner
+
+
+my_car = Car(300, 'red')
+my_airplane = Airplane()
+my_ship = Ship(500, 12, 3000)
+
+print(dir(my_car))
+print(my_car.get_is_electric())
+print(my_car.weight, my_car.max_speed, my_car.engine_power, my_car.color)
+print(my_ship.weight, my_ship.max_speed, my_ship.engine_power, my_ship.displacement)
