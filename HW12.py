e033a1ac79650225f62b6fcac75a8f4369b001e2
SanTisLugansk <alex.tischik@i.ua>
2023-06-25 20:38:15 +0300

    ДЗ 12 іиконано

--- HW12.py
+++ HW12.py
@@ -18,6 +18,8 @@ class Triangle:
     apex_b = property()
     apex_c = property()
 
+    current_apex = None
+
     @apex_a.getter
     def apex_a(self):
         return self._apex_a
@@ -57,14 +59,6 @@ class Triangle:
         else:
              raise TypeError
 
-    # def __init__(self, arg_apex_a, arg_apex_b, arg_apex_c):
-    #     if isinstance(arg_apex_a, Point) and isinstance(arg_apex_b, Point) and isinstance(arg_apex_c, Point):
-    #         self.apex_a = arg_apex_a
-    #         self.apex_b = arg_apex_b
-    #         self.apex_c = arg_apex_c
-    #     else:
-    #         raise TypeError
-
     def __init__(self, arg_apex_a, arg_apex_b, arg_apex_c):
         self.apex_a = arg_apex_a
         self.apex_b = arg_apex_b
@@ -73,10 +67,31 @@ class Triangle:
     def __str__(self):
         return f'Triangle with apex: {self.apex_a}, {self.apex_b}, {self.apex_c}'
 
+    # у такому вигляді працює тільки якщо немає вкладених циклів for
+    # def __iter__(self):
+    #     self.current_apex = self.__class__.current_apex
+    #     return self
+    #
+    # def __next__(self):
+    #     if id(self.current_apex) == id(self.apex_a):
+    #         self.current_apex = self.apex_b
+    #     elif id(self.current_apex) == id(self.apex_b):
+    #         self.current_apex = self.apex_c
+    #     elif id(self.current_apex) == id(self.apex_c):
+    #         raise StopIteration
+    #     else:
+    #         self.current_apex = self.apex_a
+    #
+    #     return self.current_apex
+
+    def __iter__(self):
+        return Triangle_iterator(self)
+
     def area(self, round_to=None):
         # короткочасні змінні можуть мати імена з однієї або двох літер, це говорить про 'тимчасовість' змінної,
         # формула площі читаеється легше при використанні коротких імен
         if self._changed:
+            print('calculate the area')
             ab = Line(self.apex_a, self.apex_b).length()
             bc = Line(self.apex_b, self.apex_c).length()
             ca = Line(self.apex_c, self.apex_a).length()
@@ -89,10 +104,45 @@ class Triangle:
         else:
             return self._area
 
-p1 = Point(0, 0)
-p2 = Point(0, 13)
-p3 = Point(3.693, 14.539)
-print(p1, p2, p3)
 
-almost_triangle_Heron = Triangle(p1, p2, p3)
-print(f'Area {almost_triangle_Heron} = {almost_triangle_Heron.area(3)}')
+class Triangle_iterator:
+    triangle = None
+    current_apex = None
+
+    def __init__(self, triangle):
+        self.triangle = triangle
+
+    # iтератор повинен повертати self у методі __iter__
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if id(self.current_apex) == id(self.triangle.apex_a):
+            self.current_apex = self.triangle.apex_b
+        elif id(self.current_apex) == id(self.triangle.apex_b):
+            self.current_apex = self.triangle.apex_c
+        elif id(self.current_apex) == id(self.triangle.apex_c):
+            raise StopIteration
+        else:
+            self.current_apex = self.triangle.apex_a
+
+        return self.current_apex
+
+
+p1 = Point(0, 2)
+p2 = Point(1, 3)
+p3 = Point(3, 9)
+# print(p1, p2, p3)
+
+my_triangle = Triangle(p1, p2, p3)
+print(f'Area {my_triangle} = {my_triangle.area(3)}')
+print(f'Area {my_triangle} = {my_triangle.area(3)}')
+
+my_triangle.apex_a = Point(2, 10)
+print(f'Area {my_triangle} = {my_triangle.area(3)}\n')
+
+for i in my_triangle:
+    print(i)
+    for j in my_triangle:
+        print(f' --- inner loop --- {j}')
+
